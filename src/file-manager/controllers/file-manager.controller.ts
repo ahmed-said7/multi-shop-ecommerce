cac539fee5a7a2218f707282f38fe881eb85dc93
mohamed saad <89225668+MohamedSaadMohamed1@users.noreply.github.com>
2024-05-13 03:07:32 +0300

    add guard to file manager

--- src/file-manager/controllers/file-manager.controller.ts
+++ src/file-manager/controllers/file-manager.controller.ts
@@ -11,13 +11,14 @@ import {
 import { FileInterceptor } from '@nestjs/platform-express';
 import { FileManagerService } from '../services/file-manager.service';
 import { UploadFileResponseDto } from '../dtos/responses/upload-file-response.dto';
+import { JwtGuard } from 'src/auth/guards/jwt-auth.guard';
 
 @Controller('file-manager')
 export class FileManagerController {
   constructor(private fileManagerService: FileManagerService) {}
 
-  //@UseGuards(JwtGuard)
-  @Post('upload-photo/:id/:isSlider') // Corrected route definition
+  @UseGuards(JwtGuard)
+  @Post('upload-photo/:id/:isSlider') 
   @UseInterceptors(FileInterceptor('file'))
   async uploadPhoto(
     @UploadedFile() file: Express.Multer.File,
@@ -25,7 +26,7 @@ export class FileManagerController {
     @Param('isSlider') isSlider: boolean,
     @Req() request: any,
   ): Promise<UploadFileResponseDto> {
-    if (!file || !id || isSlider === undefined) { // Corrected condition for isSlider
+    if (!file || !id || isSlider === undefined) { 
       throw new BadRequestException('Missing required parameters: file, id, isSlider');
     }
    
