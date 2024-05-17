08ead153a072fb8c5f5b3d55fa9b0c215d53ade4
ahmed-mohamed-eng <ahmed.mohamed.eng05@gmail.com>
2024-05-17 06:50:35 +0300

    fixed the missing module

--- src/cart/cart.module.ts
+++ src/cart/cart.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
 import { CartService } from './cart.service';
 import { CartController } from './cart.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { CartItem, CartItemSchema } from './schemas/cart-item.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: CartItem.name, schema: CartItemSchema },
+    ]),
+  ],
   controllers: [CartController],
   providers: [CartService],
 })
