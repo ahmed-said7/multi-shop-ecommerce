2a11d88d738b8dbbc439fa4a9aa683f055d2545f
Ahmed Karmy <ahmed.mohamed.eng05@gmail.com>
2024-05-24 17:44:35 +0300

    fixed the schemas

--- src/cart/cart.module.ts
+++ src/cart/cart.module.ts
@@ -4,10 +4,11 @@ import { CartController } from './cart.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Cart, CartSchema } from './schemas/cart.schema';
 import { User, UserSchema } from 'src/user/schemas/user_schema';
+import { Shop, ShopSchema, } from 'src/shop/schemas/shop_schema';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: Cart.name, schema: CartSchema } ,{ name: User.name, schema: UserSchema }]),
+    MongooseModule.forFeature([{ name: Cart.name, schema: CartSchema } ,{ name: User.name, schema: UserSchema }, {name: Shop.name, schema: ShopSchema}]),
     
   ],
   controllers: [CartController],
