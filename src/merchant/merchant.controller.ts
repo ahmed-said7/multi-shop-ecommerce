077130a24a7e0c54429cdc0ab37a0b5ed1f90c65
ahmed-mohamed-eng <ahmed.mohamed.eng05@gmail.com>
2024-06-12 01:54:36 +0300

    added the base CRUD operations to the merchant

--- src/merchant/merchant.controller.ts
+++ src/merchant/merchant.controller.ts
@@ -1,7 +1,44 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
+
 import { MerchantService } from './merchant.service';
+import { CreateMerchantDto } from './dto/createMerchant.dto';
+import { UpdateMerchantDto } from './dto/updateMerchant.dto';
 
 @Controller('merchant')
 export class MerchantController {
   constructor(private readonly merchantService: MerchantService) {}
+
+  @Post()
+  create(@Body() data: CreateMerchantDto) {
+    return this.merchantService.create(data);
+  }
+
+  @Get()
+  findAll(@Query('page') page?: number) {
+    return this.merchantService.findAll(page);
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.merchantService.findOne(id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() data: UpdateMerchantDto) {
+    return this.merchantService.update(id, data);
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    return this.merchantService.delete(id);
+  }
 }
