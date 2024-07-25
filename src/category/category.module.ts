45664d3844e2811e9c9760263d1a160ea9b6ca7b
ahmed-said7 <engahmedsaid125@gmail.com>
2024-07-25 21:50:43 +0300

    custom category

--- src/category/category.module.ts
+++ src/category/category.module.ts
@@ -30,7 +30,7 @@ import { Banner, BannerSchema } from 'src/banner/schemas/banner_schema';
 import {
   IntroPage,
   IntroPageSchema,
-} from 'src/i18n/intro-page/schemas/intro_page_schema';
+} from 'src/intro-page/schemas/intro_page_schema';
 import { Merchant, merchantSchema } from 'src/merchant/schema/merchant.schema';
 import { ApiModule } from 'src/common/filter/api.module';
 import { UploadModule } from 'src/upload/upload.module';
